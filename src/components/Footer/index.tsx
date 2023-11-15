9277879577eea290452214cc8ee628ca9a675da9
Diogo Scohati <diogoscohati@outlook.com>
2023-11-15 13:19:12 -0300

    [feat]: Footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,8 +1,9 @@
 import { Container, Group, ActionIcon, rem, Text } from '@mantine/core';
-import { IconBrandTwitter, IconBrandYoutube, IconBrandInstagram } from '@tabler/icons-react';
+import { IconBrandLinkedin, IconBrandGithub } from '@tabler/icons-react';
 import { MantineLogo } from '@mantine/ds';
 import classes from './FooterSocial.module.css';
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 const FooterSocial: React.FC = () => {
   return (
@@ -11,13 +12,14 @@ const FooterSocial: React.FC = () => {
         <h2 className='font-bold text-white'>Scohati TV</h2>
         <Group gap={0} className={classes.links} justify="flex-end" wrap="nowrap">
           <ActionIcon size="lg" color="gray" variant="subtle">
-            <IconBrandTwitter style={{ width: rem(18), height: rem(18) }} stroke={1.5} />
+            <Link to={'https://www.linkedin.com/in/scohati/'}>
+                <IconBrandLinkedin style={{ width: rem(18), height: rem(18) }} stroke={1.5} />
+            </Link>
           </ActionIcon>
           <ActionIcon size="lg" color="gray" variant="subtle">
-            <IconBrandYoutube style={{ width: rem(18), height: rem(18) }} stroke={1.5} />
-          </ActionIcon>
-          <ActionIcon size="lg" color="gray" variant="subtle">
-            <IconBrandInstagram style={{ width: rem(18), height: rem(18) }} stroke={1.5} />
+            <Link to={'https://github.com/Scohati/Scohati-TV'}>
+                <IconBrandGithub style={{ width: rem(18), height: rem(18) }} stroke={1.5} />
+            </Link>
           </ActionIcon>
         </Group>
       </Container>
