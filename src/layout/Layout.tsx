165dbce4c6a1324acdee79907300ca067baf4738
Diogo Scohati <diogoscohati@outlook.com>
2023-11-15 13:21:55 -0300

    [feat]: Layout

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { HeaderMegaMenu } from '../components/Header/Header'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import Home from '../views/Home'
 import Contato from '../views/Contato'
 import Footer from '../components/Footer'
@@ -14,6 +14,7 @@ const Layout: React.FC = () => {
                 <Routes>
                     <Route path='/' element={<Home />} />
                     <Route path="/contato" element={<Contato />} />
+                    <Route path='*' element={<Navigate to={'/'}/>}/>
                 </Routes>
                 <Footer />
             </section>
