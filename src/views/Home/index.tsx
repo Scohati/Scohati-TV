27dfce37cec3dcb9186a3fe14fb0f73b23c48998
Diogo Scohati <diogoscohati@outlook.com>
2023-11-15 14:13:07 -0300

    [feat]: Avaliados

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,31 +1,16 @@
-import React, { useState } from 'react';
-import { Skeleton } from '@mantine/core';
+import React from 'react';
 import Banner from './_components/Carousel';
 import ScrollCarouselComponent from './_components/ScrollCarousel';
 import Lancamentos from './_components/Lancamentos';
+import AvaliadosComponent from './_components/Avaliados';
 
 const Home: React.FC = () => {
   return (
     <section className='w-full my-12 px-[12vw] flex flex-col gap-8'>
-
-      <Banner page={1}/>
+      <Banner/>
       <ScrollCarouselComponent />
+      <AvaliadosComponent />
       <Lancamentos />
-
-      <h2 className='font-bold text-white text-2xl'>| Avaliados</h2>
-      <section className='w-full max-w-[1600px] h-[500px] justify-center flex gap-4'>
-        <section className='w-2/5 h-full flex flex-col gap-4'>
-          <div className='h-full'>
-            <Skeleton className='h-full' radius={'md'}/>
-          </div>
-          <div className='h-full'>
-            <Skeleton className='h-full' radius={'md'}/>
-          </div>
-        </section>
-        <div className='w-4/5 h-full'>
-          <Skeleton className='h-full' radius={'md'}/>
-        </div>
-      </section>
     </section>
   );
 }
