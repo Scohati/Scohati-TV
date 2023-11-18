81827f9df929bc6a454a36be34fb4e377dc36045
Diogo Scohati <diogoscohati@outlook.com>
2023-11-18 17:02:40 -0300

    [feat]: Chamadas por ID

--- src/services/Filmes/index.tsx
+++ src/services/Filmes/index.tsx
@@ -5,7 +5,6 @@ async function getAbstract(url: string)  {
     return data
 }
 
-
 export const getEmCartazService = async (page: number) => {
     return await getAbstract(`movie/now_playing?language=pt-br&page=${page}`)
 }
@@ -16,4 +15,19 @@ export const popularesService = async (page: number) => {
 
 export const avaliadosService = async (page: number) => {
     return await getAbstract(`movie/top_rated?language=pt-BR&page=${page}`)
+}
+
+export const getById = {
+    async get (id: number) {
+        return await getAbstract(`movie/${id}?language=pt-br`)
+    },
+    async getRecomendacoes(id: number) {
+        return await getAbstract(`movie/${id}/recommendations?language=pt-BR&page=1`)
+    },
+    async getSimilares(id: number) {
+        return await getAbstract(`movie/${id}/similar?language=en-US&page=1`)
+    },
+    async getTrailer(id: number) {
+        return await getAbstract(`movie/${id}/videos?language=pt-br`)
+    },
 }
