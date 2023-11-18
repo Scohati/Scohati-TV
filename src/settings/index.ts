d7f0e109ab1d5babcdb9daaadccf78472c6d3791
Diogo Scohati <diogoscohati@outlook.com>
2023-11-18 17:03:07 -0300

    [feat]: Rota para imagens

--- src/settings/index.ts
+++ src/settings/index.ts
@@ -1,11 +1,13 @@
 interface Settings {
     API_BASE_URL: string | undefined;
     token: string | undefined;
+    API_IMAGE_URL: string | undefined;
   }
   
   const settings: Settings = {
     API_BASE_URL: process.env.REACT_APP_SCOHATI_TV_API_URL,
     token: process.env.REACT_APP_SCOHATI_TV_API_TOKEN,
+    API_IMAGE_URL: process.env.REACT_APP_SCOHATI_TV_API_IMAGE_URL
   };
   
   export default settings;
